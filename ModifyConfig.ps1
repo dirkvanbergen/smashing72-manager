359406449d3299eaaca8bb9c7dd5083c48276b0d
dirkvanbergen <dirkvanbergen@gmail.com>
2019-03-18 20:27:43 +0100

    scripts

---
+++ ModifyConfig.ps1
@@ -0,0 +1,6 @@
+﻿Param($config, $key, $value)
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($config)
+$node = $doc.SelectSingleNode('configuration/appSettings/add[@key="' + $key + '"]')
+$node.Attributes['value'].Value = $value
+$doc.Save($config)
---
+++ ModifyConnectionString.ps1
@@ -0,0 +1,6 @@
+﻿Param($config, $key, $value)
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($config)
+$node = $doc.SelectSingleNode('configuration/connectionStrings/add[@name="' + $key + '"]')
+$node.Attributes['connectionString'].Value = $value
+$doc.Save($config)
